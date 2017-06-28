d51791282d5d6b41e60eefa72a766258615c141b
Unknown <MitchvanWijngaarden@gmail.com>
2017-06-28 10:25:36 +0200

    Changed complain to show date.

--- src/main/java/nl/allergieradar/persistence/ComplaintDAO.java
+++ src/main/java/nl/allergieradar/persistence/ComplaintDAO.java
@@ -48,7 +48,7 @@ public class ComplaintDAO extends DatabaseDAO {
             while (rs.next()) {
                 Complaint complaint = new Complaint();
                 complaint.setComplaintID(rs.getInt(1));
-//                complaint.setData(rs.getDate(2));
+                complaint.setDate(rs.getDate(2));
                 complaint.setEyes(rs.getInt(3));
                 complaint.setNose(rs.getInt(4));
                 complaint.setLungs(rs.getInt(5));
